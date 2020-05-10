6780f650e8239ff8483a77320b007e08b41c7328
lovelifealot <peredat4ik@gmail.com>
2020-05-10 19:59:53 +0500

    Fixes PtypInteger16 and PtypInteger32 types

--- src/property/property_type/PtypInteger16.ts
+++ src/property/property_type/PtypInteger16.ts
@@ -8,8 +8,7 @@ export class PtypInteger16 extends PropertyType<number> {
         super(0x0002, "PtypInteger16");
     }
     resolveValue(container: MessageStorage, propertyTag: PropertyTag): number {
-        return
-            Long.fromBytesLE(
+        return Long.fromBytesLE(
                 container.propertiesStream().properties().find(propertyInfo => PropertyTag.equal(propertyInfo.propertyTag(), propertyTag)).data().slice(0, 4)
             ).toNumber();
     }
